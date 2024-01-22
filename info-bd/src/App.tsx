5467989241f4549ff5ed249ad84a14dd98868c85
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-01-22 12:58:01 +0600

    setup redux

---
+++ info-bd/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import AllPosts from './component/AllPosts'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <AllPosts />
+    </>
+  )
+}
+
+export default App
--- info-bd/src/Redux/store.tsx
+++ info-bd/src/Redux/store.tsx
@@ -1,23 +1,20 @@
 import { applyMiddleware, compose, createStore } from "redux";
-import rootReducer from './reducers/index';
 import { thunk } from "redux-thunk";
+import rootReducer from "./reducers";
 
 declare global {
     interface Window {
-      __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
     }
 }
 
-const middleware = [thunk];
+const middleware = [thunk]
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// const store = createStore(rootReducer, applyMiddleware(thunk))
 const store = createStore(
     rootReducer,
     composeEnhancers(applyMiddleware(...middleware))
 )
 
-  export default store
-
-  
+export default store;
